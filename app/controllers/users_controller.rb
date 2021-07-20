b5aa3602f6d0f5f6e9e8c1d7007142aff0eabb60
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-20 12:46:26 +0100

    Add helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @friends = Friendship.where('friend_id = ? and status = ?', current_user.id, true) 
+    @friends = Friendship.where('friend_id = ? and status = ?', current_user.id, true)
     @friends.all.map(&:user_id)
-    @all_friends = [] 
+    @all_friends = []
     @friends.each { |u| @all_friends << User.find(u.user_id) }
   end
 
-  def confirm 
-    user = User.find(params[:friend_id]) 
-    friend = Friendship.where('friend_id = ? and user_id = ?', current_user.id, user.id).first 
-    friend.status = true 
+  def confirm
+    user = User.find(params[:friend_id])
+    friend = Friendship.where('friend_id = ? and user_id = ?', current_user.id, user.id).first
+    friend.status = true
     if friend.save
       @add_friend = Friendship.new(user_id: current_user.id, friend_id: user.id, status: true)
       @add_friend.save
